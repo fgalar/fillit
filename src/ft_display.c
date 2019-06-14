ca67054b0136f949f0f8f82fcaf8bf8ab5c102d1
Fanny <fgaraultlarpent@yahoo.com>
2019-06-14 13:26:05 +0200

    Maj main/gestion error/bool color

--- src/ft_display.c
+++ src/ft_display.c
@@ -6,45 +6,24 @@
 /*   By: fgarault <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 21:30:55 by fgarault          #+#    #+#             */
-/*   Updated: 2019/06/13 21:00:47 by fgarault         ###   ########.fr       */
+/*   Updated: 2019/06/12 18:19:03 by fgarault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "../include/fillit.h"
 #include "../libft/libft.h"
-
-
-void    ft_rainbow(char **color)
-{
-	int     	i;
-	i = 0;
-	color = (char**)malloc(sizeof(char*) * 27);
-	while (i < 27)
-	{
-		color[i] = (char*)malloc(sizeof(char) * 34);
-		color[i][33]='\0';
-		i++;
-	}
-}
 
 void	ft_display(t_mappy map)
 {
 	int		j;
 	int		i;
-	static char	*unicorn[27] = {POINT, A, B, C, D, E, F, G, H, I, J, K, L, M,
-		N, O, P, Q, R, S, T U, V, W, X, Y, Z};
 
 	j = 0;
 	while (j < map.size_map)
 	{
-		i = -1;
-		while (++i < map.size_map)
-		{
-			k = 0;
-			while (map.map[j][i] != unicorn[k])
+		i = 0;
+		while (i < map.size_map)
 			ft_putchar(map.map[j][i++]);
-		}
 		ft_putchar('\n');
 		j++;
 	}
--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.c                                           :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdavid <mdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 19:11:15 by mdavid            #+#    #+#             */
-/*   Updated: 2019/06/13 20:53:56 by fgarault         ###   ########.fr       */
+/*   Updated: 2019/06/14 13:24:18 by fgarault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,41 @@
 #include "../include/fillit.h"
 #include "../libft/libft.h"
 
-
-int     main(int ac, char **av)
+static int	ft_error(int ac, int fd)
 {
-    int         fd;
-    int         nb_tetri;
-    t_tetri     *lst_tetri;
-	t_mappy		map;
+	int		nb_tetri;
 
-    nb_tetri = 0;
-    fd = open(av[1], O_RDONLY);
-    if (ac != 2)
-    {
-        ft_putendl("usage :");
-        return (-1);
-    }
-    if (fd == -1)
-    {
-        ft_putendl("error");
-        return (-1);
-    }
-    nb_tetri = ft_check(&fd);
-    close(fd);
-    if (nb_tetri > 0)
-        lst_tetri = ft_parsing(av[1]);
+	if (ac != 2)
+	{
+		ft_putendl("usage : ./fillit [input file]");
+		return (-1);
+	}
+	if (fd == -1 || ((nb_tetri = ft_check(&fd)) == -1))
+	{
+		ft_putendl("error");
+		return (-1);
+	}
+	return (nb_tetri);
+}
+
+int			main(int ac, char **av)
+{
+	int		fd;
+	int		nb_tetri;
+	t_tetri	*lst_tetri;
+	t_mappy	map;
+	int		bool_c;
+
+	fd = open(av[1], O_RDONLY);
+	bool_c = 1;
+	if ((nb_tetri = ft_error(ac, fd)) == -1)
+		return (0);
+	lst_tetri = ft_parsing(av[1]);
 	map.size_map = map_min(nb_tetri);
-    if (!(map.map = mapper(26)))
+	if (!(map.map = mapper(26)))
 		return (-1);
 	while (!(bck_trck(lst_tetri, map)))
 		(map.size_map)++;
-	ft_display(map);
+	bool_c == 1 ? ft_display(map) : ft_display_unicorn(map);
 	return (0);
-} 
+}
